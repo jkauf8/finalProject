92b21cfc43eca10a951164719412169297582287
justinkaufman <justinkaufman@172.19.202.69>
2019-04-19 08:18:56 -0700

    paddle fixed

--- pongLab/src/ponglab/Paddle.java
+++ pongLab/src/ponglab/Paddle.java
@@ -13,36 +13,76 @@ public class Paddle extends Block
 
    public Paddle()
    {
-		super(10,10);
+		super(40,40);
       speed =5;
    }
 
 
    //add the other Paddle constructors
-
-
-
-
-
-
-
-
+   public Paddle(int s, int t)
+   {
+	   super(s,t);
+	   speed = 5;
+   }
+   public Paddle(int s, int t, int z)
+   {
+	   super(s,t);
+	   speed = z;
+   }
+   public Paddle(int s, int t, int z, int w)
+   {
+	   super(s,t,z,w);
+	   speed = 5;
+   }
+   public Paddle(int s, int t, int z, int w, int g)
+   {
+	   super(s,t,z,w);
+	   speed = g;
+   }
+   public Paddle(int s, int t, int z, int w, Color co,int g)
+   {
+	   super(s,t,z,w,co);
+	   speed = g;
+   }
 
 
    public void moveUpAndDraw(Graphics window)
    {
-
-
+        draw(window,Color.WHITE);
+        setY(getY()-speed);
+	draw(window);
    }
 
    public void moveDownAndDraw(Graphics window)
    {
-
-
+        draw(window,Color.WHITE);
+        setY(getY()+speed);
+	draw(window);
+   }
+   public void moveLeftAndDraw(Graphics window)
+   {
+	draw(window,Color.WHITE);
+	setX(getX()-speed);
+	draw(window);
+   }
+   public void moveRightAndDraw(Graphics window)
+   {
+	draw(window,Color.WHITE);
+	setX(getX()+speed);
+	draw(window);
    }
 
    //add get methods
-   
+   public int getSpeed()
+   {
+	return speed;   
+   }
    
    //add a toString() method
+   public String toString()
+   {
+	String output ="";
+	output+= super.toString() + " " + getSpeed();
+	return output;
+   }
 }
